64c47195c4c0a912ce02685c2db0c7fa85b55cfe
Uriel Cohen <uriel.c@sealsecurity.io>
2025-09-04 09:48:45 +0300

    CVE-2023-44487

--- java/org/apache/coyote/http2/Http2UpgradeHandler.java
+++ java/org/apache/coyote/http2/Http2UpgradeHandler.java
@@ -375,7 +375,7 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
                                     stream.close(se);
                                 }
                             } finally {
-                                if (overheadCount.get() > 0) {
+                                if (isOverheadLimitExceeded()) {
                                     throw new ConnectionException(
                                             sm.getString("upgradeHandler.tooMuchOverhead", connectionId),
                                             Http2Error.ENHANCE_YOUR_CALM);
@@ -1507,6 +1507,9 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
         }
     }
 
+    boolean isOverheadLimitExceeded() {
+        return overheadCount.get() > 0;
+    }
 
     // ----------------------------------------------- Http2Parser.Input methods
 
@@ -1779,6 +1782,7 @@ class Http2UpgradeHandler extends AbstractStream implements InternalHttpUpgradeH
             log.debug(sm.getString("upgradeHandler.reset.receive", getConnectionId(), Integer.toString(streamId),
                     Long.toString(errorCode)));
         }
+        increaseOverheadCount(FrameType.RST, getProtocol().getOverheadResetFactor());
         AbstractNonZeroStream abstractNonZeroStream = getAbstractNonZeroStream(streamId, true);
         abstractNonZeroStream.checkState(FrameType.RST);
         if (abstractNonZeroStream instanceof Stream) {
